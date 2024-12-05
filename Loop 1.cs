5851d58c0606f6b69bb7b4ac7db6d94192467213
budnyinqa <budnyinqa@gmail.com>
2024-12-05 16:04:02 +0100

    Add files via upload

---
+++ Loop 1.cs
@@ -0,0 +1,14 @@
+﻿// Printing numbers divisible by 3 or 5 in the range of 1 to 100
+using System;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0) Console.WriteLine(i);
+        }
+    }
+}
